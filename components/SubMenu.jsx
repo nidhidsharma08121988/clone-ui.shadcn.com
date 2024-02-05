23394edcf847613d53b281f524206611c185bc8e
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-02-05 10:39:06 +0400

    Update: Changes made to the SubMenu component. 1. Since we want dynamic classes we change this component to client component using 'use client' 2. Assign class based on the  path that the user is currently on

--- components/SubMenu.jsx
+++ components/SubMenu.jsx
@@ -1,46 +1,107 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const SubMenu = () => {
-  const menuItems = [
-    { path: 'mail', name: 'Mail' },
-    {
-      path: 'dashboard',
-      name: 'Dashboard',
-    },
-    { path: 'cards', name: 'Cards' },
-    { path: 'tasks', name: 'Tasks' },
-    {
-      path: 'playground',
-      name: 'Playground',
-    },
-    { path: 'forms', name: 'Forms' },
-    { path: 'music', name: 'Music' },
-    {
-      path: 'authentication',
-      name: 'Authentication',
-    },
-  ];
-  // const selectedClassNames = "flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary bg-muted font-medium text-primary";
-  const classNames =
-    'flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary';
-  // const classes = isSelected?
-  // 'bg-muted font-medium text-primary':"text-muted-foreground';
+  const selectedClassNames =
+    'bg-muted font-medium text-primary';
+  const unSelectedClassNames =
+    ' text-muted-foreground hover:text-primary';
+ const commonClassNames='flex h-7 items-center text-sm justify-center rounded-full px-4 text-center transition-colors';
+
   return (
     <div className='min-w-full table'>
-      <ul className='mb-4 flex items-center'>
-        {menuItems.map(
-          (menuItem, index) => (
-            <Link
-              key={index}
-              className={classNames}
-              href={`/examples/${menuItem.path}`}
-            >
-              {menuItem.name}
-            </Link>
-          )
-        )}
-      </ul>
+      <div className='mb-4 flex items-center'>
+        <Link
+          className={`${
+            usePathname() === '/examples/mail'
+              ? selectedClassNames 
+              : unSelectedClassNames
+          }  ${commonClassNames}`}
+          href='/examples/mail'
+          scroll={false}
+        >
+          Mail
+        </Link>
+        <Link
+         className={`${
+             usePathname() === '/examples/dashboard'
+              ? selectedClassNames 
+              : unSelectedClassNames
+          }  ${commonClassNames}`}
+          href='/examples/dashboard'
+          scroll={false}
+        >
+          Dashboard
+        </Link>
+        <Link
+         className={`${
+           usePathname() ==='/examples/cards'
+              ? selectedClassNames 
+              : unSelectedClassNames
+          }  ${commonClassNames}`}
+          href='/examples/cards'
+          scroll={false}
+        >
+          Cards
+        </Link>
+        <Link
+         className={`${
+             usePathname() === '/examples/tasks'
+              ? selectedClassNames 
+              : unSelectedClassNames
+          }  ${commonClassNames}`}
+          href='/examples/tasks'
+          scroll={false}
+        >
+          Tasks
+        </Link>
+        <Link
+         className={`${
+             usePathname() === '/examples/playground'
+              ? selectedClassNames 
+              : unSelectedClassNames
+          }  ${commonClassNames}`}
+          href='/examples/playground'
+          scroll={false}
+        >
+          Playground
+        </Link>
+        <Link
+          className={`${
+             usePathname() === '/examples/forms'
+              ? selectedClassNames 
+              : unSelectedClassNames
+          }  ${commonClassNames}`}
+          href='/examples/forms'
+          scroll={false}
+        >
+          Forms
+        </Link>
+        <Link
+         className={`${
+             usePathname() === '/examples/music'
+              ? selectedClassNames 
+              : unSelectedClassNames
+          }  ${commonClassNames}`}
+          href='/examples/music'
+          scroll={false}
+        >
+          Music
+        </Link>
+        <Link
+         className={`${
+            usePathname() === '/examples/authentication'
+              ? selectedClassNames 
+              : unSelectedClassNames
+          }  ${commonClassNames}`}
+          href='/examples/authentication'
+          scroll={false}
+        >
+          Authentication
+        </Link>
+      </div>
     </div>
   );
 };
